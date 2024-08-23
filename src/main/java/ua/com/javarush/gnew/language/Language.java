6cd28f2bb912fcd5d2baa95e90553f1492da24db
oleksandr-jr <oleksandr.cho@gmail.com>
2024-08-23 18:48:36 +0200

    Changed the alphabet constructor

--- src/main/java/ua/com/javarush/gnew/language/Language.java
+++ src/main/java/ua/com/javarush/gnew/language/Language.java
@@ -1,13 +1,12 @@
 package ua.com.javarush.gnew.language;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public abstract class Language {
 
     private final ArrayList<Character> alphabet;
 
     public Language(ArrayList<Character> alphabet) {
-        this.alphabet = new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
+        this.alphabet = alphabet;
     }
 }
